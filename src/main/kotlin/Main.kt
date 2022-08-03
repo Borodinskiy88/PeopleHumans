05aac4f8951fa532db7b3290a8c5e8ceee8ae45d
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-03 21:13:00 +0300

    Готовое

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,11 @@ package ru.netology
 fun main() {
     val likes = 8
     val message = if (likes % 10 == 1 && likes % 100 != 11) {
-        "Это понравилось " + likes + " человеку!"
+        "Это понравилось $likes человеку!"
     } else if (likes == 0) {
         "Пока нет оценок"
     } else {
-        "Это понравилось " + likes + " людям!"
+        "Это понравилось $likes людям!"
     }
     println(message)
 }
