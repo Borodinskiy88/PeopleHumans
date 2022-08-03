8158942e7b39981b5ac5c169c6acfb263814aa68
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-08-03 20:27:06 +0300

    Готовое

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    var likes = 8
+    val likes = 8
     val message = if (likes % 10 == 1 && likes % 100 != 11) {
         "Это понравилось " + likes + " человеку!"
     } else if (likes == 0) {
